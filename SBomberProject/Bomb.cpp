4d07f2d3f5c0251c80c32ac2c5b83b8fda557e18
Ilya_Kiselev <mybutifullie@gmail.com>
2022-02-10 10:06:40 +1000

    added bomb decorator

--- SBomberProject/Bomb.cpp
+++ SBomberProject/Bomb.cpp
@@ -13,3 +13,41 @@ void Bomb::Draw() const
     ScreenSingleton::getInstance().GotoXY(x, y);
     cout << "*";
 }
+
+void BombDecorator::Move(uint16_t time)
+{
+	bomb.Move(time * 1.5);
+}
+
+void BombDecorator::Draw() const
+{
+	bomb.Draw();
+
+	ScreenSingleton::getInstance().GotoXY(bomb.GetX(), bomb.GetY() - 1);
+	cout << "|";
+}
+
+void BombDecorator::SetPos(double nx, double ny)
+{
+	bomb.SetPos(nx, ny);
+}
+
+uint16_t BombDecorator::GetWidth() const
+{
+	return bomb.GetWidth();
+}
+
+double BombDecorator::GetY() const
+{
+	return bomb.GetY();
+}
+
+double BombDecorator::GetX() const
+{
+	return bomb.GetX();
+}
+
+void BombDecorator::SetWidth(uint16_t widthN)
+{
+	bomb.SetWidth(widthN);
+}
